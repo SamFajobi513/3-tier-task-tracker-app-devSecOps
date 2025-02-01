25bf6b581b5987b53fb36c3c18938c06ec29afda
SamFajobi <olusegunfajobi12@gmail.com>
2025-02-02 00:58:44 +0100

    thrift backend-initiated

---
+++ app-source-code/backend/src/database.ts
@@ -0,0 +1,4 @@
+import mongoose from 'mongoose'
+const ENVIRONMENT: any = process.env.ENVIRONMENT
+const MONGODB_LOCAL_URL: any = process.env.MONGODB_LOCAL_URL
+const MONGODB_LIVE_URL: any = process.env.MONGODB_LIVE_URL
