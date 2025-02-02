6610225797689e5298bc1e67b04ac763d0e7a40d
SamFajobi <olusegunfajobi12@gmail.com>
2025-02-02 01:12:33 +0100

    server_setup

--- app-source-code/backend/src/server.ts
+++ app-source-code/backend/src/server.ts
@@ -10,3 +10,12 @@ import agentRoute from './routes/agent.routes';
 import adminRoute from './routes/admin.routes';
 import superAdminRoute from './routes/superadmin.routes';
 import userRoute from './routes/user.routes';
+
+const api: string = '/api/v1'
+const app: Application = express()
+const PORT: any = process.env.PORT || 5120
+
+app.use(morgan("combined"));
+app.use(cors())
+app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ limit: '100mb', extended: false }));
