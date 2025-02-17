31d10d994362bc3c313ac27b251434c3f67afa2a
SamFajobi <olusegunfajobi12@gmail.com>
2025-02-17 03:03:46 +0100

    ec2-created

--- terraform-code/ec2.tf
+++ terraform-code/ec2.tf
@@ -1,16 +1,48 @@
 resource "aws_instance" "ec2" {
-  ami                    = data.aws_ami.ami.image_id
-  instance_type          = "t2.2xlarge"
+  ami                    = var.ami
+  instance_type          = var.instance_type
   key_name               = var.key-name
-  subnet_id              = aws_subnet.public-subnet.id
+  subnet_id              = var.subnet-id
   vpc_security_group_ids = [aws_security_group.security-group.id]
-  iam_instance_profile   = aws_iam_instance_profile.instance-profile.name
   root_block_device {
-    volume_size = 30
+    volume_size = 15
   }
-  user_data = templatefile("./tools-install.sh", {})
+  user_data = templatefile("./install.sh", {})
 
   tags = {
     Name = var.instance-name
   }
 }
+
+
+
+resource "aws_security_group" "security-group" {
+  name        = var.sg-name
+  description = "Allowing Jenkins, Sonarqube, SSH Access and TLS traffic"
+
+  ingress = [
+    for port in [22, 8080, 9000, 9090, 80] : {
+      description      = "TLS from VPC"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      ipv6_cidr_blocks = ["::/0"]
+      self             = false
+      prefix_list_ids  = []
+      security_groups  = []
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  ]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.sg-name
+  }
+}
+
---
+++ terraform-code/variables.tf
@@ -0,0 +1,25 @@
+variable "aws-region" {}
+variable "key-name" {}
+variable "ami" {}
+variable "instance_type" {}
+variable "sg-name" {}
+variable "instance-name" {}
+variable "subnet-id" {}
+# variable "env" {}
+# variable "cluster-name" {}
+# variable "vpc-cidr" {}
+# variable "vpc-name" {}
+# variable "igw-name" {}
+# variable "pub-subnet-count" {}
+# variable "pub-cidr-block" {}
+# variable "pub-availability-zone" {}
+# variable "pub-sub-name" {}
+# variable "pri-subnet-count" {}
+# variable "pri-availability-zone" {}
+# variable "pri-sub-name" {}
+# variable "public-rt-name" {}
+# variable "private-rt-name" {}
+# variable "pri-cidr-block" {}
+# variable "eoigw" {}
+# variable "eip-name" {}
+# variable "ngw-name" {}
